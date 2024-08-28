8573e5d9dc32b5810d099665698b0f7b9295dfcd
안정후 <austinan@naver.com>
2024-08-28 17:40:20 +0900

    chore: for controller, dto, converter

--- inbounds/src/main/java/com/pocket/inbounds/InboundsApplication.java
+++ inbounds/src/main/java/com/pocket/inbounds/InboundsApplication.java
@@ -1,4 +1,4 @@
-package com.foodgo.inbounds;
+package com.pocket.inbounds;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- inbounds/src/test/java/com/pocket/inbounds/InboundsApplicationTests.java
+++ inbounds/src/test/java/com/pocket/inbounds/InboundsApplicationTests.java
@@ -1,4 +1,4 @@
-package com.foodgo.inbounds;
+package com.pocket.inbounds;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
