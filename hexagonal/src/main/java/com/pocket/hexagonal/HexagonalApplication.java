3bcfa61546c91a59a283648581b10579e210f1a7
안정후 <austinan@naver.com>
2024-08-29 11:22:22 +0900

    feat: make entity scan

--- hexagonal/src/main/java/com/pocket/hexagonal/HexagonalApplication.java
+++ hexagonal/src/main/java/com/pocket/hexagonal/HexagonalApplication.java
@@ -2,8 +2,10 @@ package com.pocket.hexagonal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.pocket")
 public class HexagonalApplication {
 
     public static void main(String[] args) {
