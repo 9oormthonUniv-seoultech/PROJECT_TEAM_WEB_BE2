de46ade15fdf1617801e10758703f2a3d14b010e
안정후 <austinan@naver.com>
2024-09-09 14:08:59 +0900

    fix: embeddable entity

--- domain/src/main/java/com/pocket/domain/entity/review/Review.java
+++ domain/src/main/java/com/pocket/domain/entity/review/Review.java
@@ -1,23 +1,21 @@
 package com.pocket.domain.entity.review;
 
-import jakarta.persistence.ElementCollection;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
+@Getter
+@Embeddable
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Review {
+
+    @Column(name = "rating")
     private int rating;
+
+    @Column(name = "content")
     private String content;
-
-    // 리뷰 요약 - 부스 : 깔끔한 소품, 예쁜 셀카존 등등
-    @Enumerated(EnumType.STRING)
-    @ElementCollection(targetClass = BoothFeature.class)
-    private List<BoothFeature> boothFeatures;
-
-    // 리뷰 요약 - 사진 : 선명한 화질, 자연스러운 보정 등등
-    @Enumerated(EnumType.STRING)
-    @ElementCollection(targetClass = PhotoFeature.class)
-    private List<PhotoFeature> photoFeatures;
 
 }
