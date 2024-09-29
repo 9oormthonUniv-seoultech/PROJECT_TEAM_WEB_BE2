e10e4a34bd641c34bc88251990ff1b77a276ecbe
안정후 <austinan@naver.com>
2024-09-29 17:08:20 +0900

    feat: domian module for near photobooth

--- domain/src/main/java/com/pocket/domain/service/photobooth/PhotoBoothFindService.java
+++ domain/src/main/java/com/pocket/domain/service/photobooth/PhotoBoothFindService.java
@@ -1,10 +1,14 @@
 package com.pocket.domain.service.photobooth;
 
+import com.pocket.domain.dto.photobooth.NearPhotoBoothInfo;
 import com.pocket.domain.dto.photobooth.PhotoBoothFindResponseDto;
+import com.pocket.domain.entity.photobooth.PhotoBoothBrand;
 import com.pocket.domain.port.photobooth.PhotoBoothFindPort;
 import com.pocket.domain.usecase.photobooth.PhotoBoothFindUseCase;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 
 @Service
@@ -15,5 +19,9 @@ public class PhotoBoothFindService implements PhotoBoothFindUseCase {
 
     public PhotoBoothFindResponseDto getPhotoBoothFindResponse(Long id) {
         return photoBoothFindPort.findById(id);
+    }
+
+    public List<NearPhotoBoothInfo> findNearPhotoBooth(double lat, double lon, PhotoBoothBrand brand) {
+        return photoBoothFindPort.getPhotoboothWithin2Km(lat, lon, brand);
     }
 }
