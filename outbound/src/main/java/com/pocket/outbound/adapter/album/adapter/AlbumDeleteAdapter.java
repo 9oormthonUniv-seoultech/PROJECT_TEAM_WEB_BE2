c2d2ff738d9cabd31b9ea1a2f8eeaa110cf86561
JiinHong <com5942@seoultech.ac.kr>
2024-11-08 17:19:26 +0900

    refactor: album 삭제 API 수정

--- outbound/src/main/java/com/pocket/outbound/adapter/album/adapter/AlbumDeleteAdapter.java
+++ outbound/src/main/java/com/pocket/outbound/adapter/album/adapter/AlbumDeleteAdapter.java
@@ -4,6 +4,7 @@ import com.pocket.core.aop.annotation.AdapterService;
 import com.pocket.domain.port.album.AlbumDeletePort;
 import com.pocket.outbound.repository.album.AlbumHashTagRepository;
 import com.pocket.outbound.repository.album.AlbumRepository;
+import com.pocket.outbound.repository.album.AlbumShareRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,10 +14,12 @@ public class AlbumDeleteAdapter implements AlbumDeletePort {
 
     private final AlbumHashTagRepository albumHashTagRepository;
     private final AlbumRepository albumRepository;
+    private final AlbumShareRepository albumShareRepository;
 
     @Override
     @Transactional
-    public void deleteAlbum(Long albumId) {
+    public void deleteAlbum(Long albumId, String userEmail) {
+        albumShareRepository.deleteByAlbumIdAndUser_User_Email(albumId, userEmail);
         albumHashTagRepository.deleteByJpaAlbum_Id(albumId);
         albumRepository.deleteById(albumId);
     }
